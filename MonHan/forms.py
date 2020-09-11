e58ce2037d90feb8364f1000deb03f6dfb7fbd7f
matsu <rice_shower_35@hotmail.com>
2020-09-11 17:07:35 +0900

    commit

--- MonHan/forms.py
+++ MonHan/forms.py
@@ -1,8 +1,14 @@
 from django import forms
 from .models import Monster
+from django.db.models.fields import BLANK_CHOICE_DASH
 
 class MonsterSearchForm(forms.Form):
     key_word = forms.CharField(
         label='モンスター名', required=False,
         widget=forms.TextInput(attrs={'class': 'input'})
     )
+    race = forms.ChoiceField(
+        label='', required=False,
+        choices=BLANK_CHOICE_DASH + list(Monster.RACE),
+    )
+    
--- MonHan/views.py
+++ MonHan/views.py
@@ -13,8 +13,13 @@ class monster_list(generic.ListView):
         queryset = super().get_queryset()
         form = MonsterSearchForm(self.request.GET or None)
         if form.is_valid():
+            # 名前で検索
             key_word = form.cleaned_data.get('key_word')
             if key_word:
                 queryset = queryset.filter(Q(name__icontains=key_word))
+            # 種別で検索
+            race = form.cleaned_data.get('race')
+            if race:
+                queryset = queryset.filter(race=race)
         
         return queryset
